6c2b37f2a99dece44323bd7599c6ecb1dafe70d9
Amandeep Singh <amanbhagtana2004@gmail.com>
2026-04-11 12:34:52 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/config/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handle(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
+        return "error";
+    }
+}
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,18 +2,80 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 @Configuration
 public class SecurityConfig {
 
+    private final AppConfig appConfig;
+
+    private final UserDetailsService userService;
+
+    public SecurityConfig(AppConfig appConfig, UserDetailsService userService) {
+        this.appConfig = appConfig;
+        this.userService = userService;
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(appConfig.passwordEncoder());
+        return authProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+
+                .authenticationProvider(authProvider())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/register",
+                                "/login",
+                                "/forgot-password",
+                                "/generate-otp",
+                                "/reset-password",
+                                "/css/**"
+                        ).permitAll()
+                        .requestMatchers("/dashboard").authenticated()
+                        .anyRequest().authenticated()
+                )
+
+
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login") // IMPORTANT
+                        .defaultSuccessUrl("/dashboard", true)
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // important
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .permitAll()
+                );
 
         return http.build();
+
+
     }
 }
