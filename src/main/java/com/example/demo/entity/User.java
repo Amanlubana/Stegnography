52f784f935878c28b4aaa63913cd98cebc1f3584
Amandeep Singh <amanbhagtana2004@gmail.com>
2026-04-11 12:36:30 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 4, max = 20, message = "Username must be 4-20 characters")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private String role = "USER";
+
+    @Column(unique = true)
+    private String email;
+
+
+}
